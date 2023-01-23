c7663f7c3afcc4367494c391d159f4e3ae67eabf
Rayan Slim <rayan.slim@cic.gc.ca>
2023-01-23 13:09:03 -0500

    first commit

---
+++ src/main/java/ca/gc/ircc/integration/statepersistence/StatePersistenceApplication.java
@@ -0,0 +1,13 @@
+package ca.gc.ircc.integration.statepersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatePersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatePersistenceApplication.class, args);
+	}
+
+}
